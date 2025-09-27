ab0d544488a590cd6e7189b7cf2d5f0645cf62eb
vaer <arkvaer@gmail.com>
2025-09-27 20:25:29 +0800

    init

---
+++ Objects/main.d
@@ -0,0 +1,5 @@
+./objects/main.o: User\main.c \
+  F:\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  F:\Keil\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm3.h \
+  F:\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
---
+++ Objects/stm32f10x_it.d
@@ -0,0 +1,5 @@
+./objects/stm32f10x_it.o: User\stm32f10x_it.c User\stm32f10x_it.h \
+  F:\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  F:\Keil\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm3.h \
+  F:\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
